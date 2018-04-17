6263acb2cccc2678bd18c82d42869290e236c1d5
subramaniashiva <vengaishiva@gmail.com>
2018-04-17 17:23:24 +0100

    feat(voucher-code): add voucher codes as constant

--- src/app/utils/app-const.ts
+++ src/app/utils/app-const.ts
@@ -2,8 +2,15 @@ export class AppConst {
   public static readonly STORE_API_PATHS = {
     getItems: '/people',
     itemDescription: '/people/{{id}}',
-    buyItems: '/people'
+    buyItems: '/people',
+    verfiyVoucher: '/people'
   };
 
   public static readonly DEFAULT_CURRENCY_SYMBOL = '£';
+
+  public static readonly VOUCHER_CODES = {
+    OFF5: '5OFF',
+    OFF10: '10OFF',
+    OFF15: '15OFF'
+  };
 }
